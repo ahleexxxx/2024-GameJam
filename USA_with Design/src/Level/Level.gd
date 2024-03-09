1b7d7235e1f5baf6f3592ba4cfb704e39736a405
ahleexxxx <122420468+ahleexxxx@users.noreply.github.com>
2024-03-09 16:18:52 -0600

    USA with design

---
+++ USA_with Design/src/Level/Level.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+#const LIMIT_LEFT = -315
+const LIMIT_LEFT = -5000
+#const LIMIT_TOP = -250
+const LIMIT_TOP = -5000
+#const LIMIT_RIGHT = 955
+const LIMIT_RIGHT = 5000
+#const LIMIT_BOTTOM = 690
+const LIMIT_BOTTOM = 5000
+
+
+func _ready():
+	for child in get_children():
+		if child is Player:
+			var camera = child.get_node("Camera")
+			camera.limit_left = LIMIT_LEFT
+			camera.limit_top = LIMIT_TOP
+			camera.limit_right = LIMIT_RIGHT
+			camera.limit_bottom = LIMIT_BOTTOM
